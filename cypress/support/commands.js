19d514e832918d42102c66e5e3b68fc645e121f9
AdamSantos27 <adamlsantos.qa@gmail.com>
2023-08-19 22:01:17 -0200

    foi incluido novo comando para limpar input

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,15 @@
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function () {
-  cy.get('#firstName').type('adam')
+  cy.get('#firstName').type('Adam')
   cy.get('#lastName').type('Santos')
   cy.get('#email').type('adamsantos@gmail.com')
   cy.get('#open-text-area').type('teste')
-  cy.contains('button', 'Enviar').click()
+  cy.get('#phone').type('8587564587')
+})
+
+Cypress.Commands.add('clearfullinputs', function () {
+  cy.get('#firstName').type('Adam',{delay:0}).clear()
+  cy.get('#lastName').type('Santos',{delay:0}).clear()
+  cy.get('#email').type('adamsantos@gmail.com',{delay:0}).clear()
+  cy.get('#open-text-area').type('teste',{delay:0}).clear()
+  cy.get('#phone').type('8587564587',{delay:0}).clear()
 })
