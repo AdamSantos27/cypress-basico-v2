e9eb50a0cac472ebc5f46c30a556539d83cf7468
AdamSantos27 <adamlsantos.qa@gmail.com>
2023-08-26 11:06:01 -0300

    incluindo a funcionalidade Cypress._.times

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,12 +1,6 @@
-/// <reference types="Cypress" />
-
-describe('Central de Atendimento ao Cliente TAT - Política de privacidade', function () {
-
-  beforeEach(function () {
-    cy.visit('./src/index.html')
-  })
-  it('', () => {
-    cy.visit('./src/privacy.html')
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
-  })
+  Cypress._.times(5, () => {
+    it('testa a página da política de privacidade de forma independente', () => {
+      cy.visit('/src/privacy.html')
+      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
+})
   })
